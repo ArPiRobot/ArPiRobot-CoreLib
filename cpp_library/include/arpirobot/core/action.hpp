a4cd80c3da75e14aad37a18d18be26717241b87d
Marcus Behel <marcus.behel@mindspring.com>
2021-01-23 00:25:57 -0500

    Doc comments in C++ action header

--- cpp_library/include/arpirobot/core/action.hpp
+++ cpp_library/include/arpirobot/core/action.hpp
@@ -10,22 +10,53 @@
 
 namespace arpirobot{
 
+    /**
+     * Generic action class. User actions should inherit this class and implement the four pure virtual methods
+     */
     class Action{
     public:
+
+        /**
+         * Use this action to lock a set of devices. 
+         * This is the same as calling Action::lockDevice once for each device individually.
+         * @param devices A vector of devices to lock
+         */
         void lockDevices(std::vector<BaseDevice*> devices);
 
+        /**
+         * Use this action to lock a device. When a device is locked by this action, 
+         * any action previously locking it will be stopped.
+         * @param device The device to lock
+         */
         void lockDevice(BaseDevice *device);
 
+        /**
+         * @returns true if the action has been started, but has not finished or been stopped.
+         */
         bool isRunning();
 
     protected:
 
+        /**
+         * Run when the action is started.
+         */
         virtual void begin() = 0;
 
+        /**
+         * Run periodically after the action is started, but before it finishes.
+         */
         virtual void process() = 0;
 
+        /**
+         * Run when the action is stopped.
+         * @param wasInterrupted Will be true if the action did not stop on its own (see Action::shouldContinue)
+         */
         virtual void finish(bool wasInterrupted) = 0;
 
+        /**
+         * Called each time after Action::process. When this returns false the action will stop.
+         * @returns false if the action should stop. Else true.
+         */
         virtual bool shouldContinue() = 0;
 
     private:
@@ -50,7 +81,10 @@ namespace arpirobot{
         friend class ActionSeries;
     };
 
-
+    /**
+     * Generic action trigger. Triggers are registered with the action manager. When some event occurs
+     * a trigger will run the designated action.
+     */
     class BaseActionTrigger{
     public:
         BaseActionTrigger(Action *targetAction, bool doRestart = true);
@@ -68,7 +102,7 @@ namespace arpirobot{
         friend class ActionManager;
     };
 
-    
+
     class ActionManager{
     public:
 
@@ -77,6 +111,7 @@ namespace arpirobot{
          * @param action    A pointer to the action to start.
          * @param doRestart If true starting an action that is already running will restart the action.
          *                  If false the action will continue running uninterrupted.
+         * @returns true if the action was started successfully
          */
         static bool startAction(Action *action, bool doRestart = true);
 
@@ -86,8 +121,18 @@ namespace arpirobot{
          */
         static void addTrigger(BaseActionTrigger *trigger);
 
+        /**
+         * Remove a trigger
+         * @param trigger A pointer to the trigger to remove
+         */
         static void removeTrigger(BaseActionTrigger *trigger);
 
+        /**
+         * Stop an action (interrupts it)
+         * If the action is not running nothing is done.
+         * @param action A pointer to the action to stop
+         * @returns true if the action was stopped. If false, the action was not running.
+         */
         static bool stopAction(Action *action);
 
     private:
@@ -105,8 +150,15 @@ namespace arpirobot{
     };
 
 
+    /**
+     * A special action that will run a sequential set of actions (one at a time)
+     */
     class ActionSeries : public Action{
     public:
+        /**
+         * @param actions A vector of actions to run sequentially
+         * @param finishedAction An action to transition to once other actions are complete
+         */
         ActionSeries(std::vector<Action*> actions, Action *finishedAction);
 
     protected:
