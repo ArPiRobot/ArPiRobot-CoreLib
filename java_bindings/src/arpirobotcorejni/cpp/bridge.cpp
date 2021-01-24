5aa865ca9c9ec19b91b229911945a337f6b6179a
Marcus Behel <marcus.behel@mindspring.com>
2021-01-24 16:47:06 -0500

    before moving to JNA

--- java_bindings/src/arpirobotcorejni/cpp/bridge.cpp
+++ java_bindings/src/arpirobotcorejni/cpp/bridge.cpp
@@ -1,5 +1,26 @@
 #include <com_arpirobot_Bridge.h>
 
-extern "C" int testFunc(){
-    return 5;
+#include <arpirobot/bridge.hpp>
+
+extern "C" {
+
+
+JNIEXPORT jlong JNICALL Java_com_arpirobot_Bridge_BaseRobot_1create(JNIEnv *env, jclass cls, jobject javaRobotObj){
+    jclass objclass = env->GetObjectClass(javaRobotObj);
+    jmethodID robotStarted = env->GetMethodID(objclass, "robotStarted", "()V");
 }
+
+JNIEXPORT void JNICALL Java_com_arpirobot_Bridge_BaseRobot_1destroy(JNIEnv *env, jclass cls, jlong ptr){
+
+}
+
+JNIEXPORT void JNICALL Java_com_arpirobot_Bridge_BaseRobot_1start(JNIEnv *env, jclass cls, jlong ptr){
+
+}
+
+JNIEXPORT void JNICALL Java_com_arpirobot_Bridge_BaseRobot_1feedWatchdog(JNIEnv *env, jclass cls, jlong ptr){
+
+}
+
+
+} // extern "C"
--- java_bindings/src/main/java/com/arpirobot/Bridge.java
+++ java_bindings/src/main/java/com/arpirobot/Bridge.java
@@ -1,8 +1,31 @@
 package com.arpirobot;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 import com.arpirobot.core.BaseRobot;
 
 public class Bridge {
+
+    static {
+        try {
+            URL url = Bridge.class.getResource("/libarpirobotcorejni.so");
+            File tempDir = Files.createTempDirectory("nativelib-extract").toFile();
+            tempDir.deleteOnExit();
+            File tempNativeLibFile = new File(tempDir, "libarpirobotcorejni.so");
+            tempNativeLibFile.deleteOnExit();
+            InputStream in = url.openStream();
+            Files.copy(in, tempNativeLibFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            System.load(tempNativeLibFile.getAbsolutePath());
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load JNI bridge library.");
+        }
+        
+    }
+
     ////////////////////////////////////////////////////////////////////////////
     /// BaseRobot Bridge
     ////////////////////////////////////////////////////////////////////////////
