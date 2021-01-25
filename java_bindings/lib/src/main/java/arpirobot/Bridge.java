84ca608469fa667ebcc056dcf2fcb9443a7e0628
Marcus Behel <marcus.behel@mindspring.com>
2021-01-24 20:03:31 -0500

    PoC build tested loading of library

--- java_bindings/lib/src/main/java/arpirobot/Bridge.java
+++ java_bindings/lib/src/main/java/arpirobot/Bridge.java
@@ -6,11 +6,12 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 public class Bridge {
-    public interface CLibrary extends Library {
-        interface vptr_t extends Callback{
-            void invoke();
-        }
 
+    public interface vptr_t extends Callback{
+        void invoke();
+    }
+
+    public interface CLibrary extends Library {
         public CLibrary instance = (CLibrary)Native.load("arpirobot-core", CLibrary.class);
 
         public long BaseRobot_create(vptr_t robotStarted, vptr_t robotEnabled, vptr_t robotDisabled, vptr_t enabledPeriodic, 
--- java_bindings/testrobot/src/main/java/robot/Main.java
+++ java_bindings/testrobot/src/main/java/robot/Main.java
@@ -3,7 +3,44 @@ package robot;
 import arpirobot.Bridge;
 
 public class Main {
-    public static void main(){
-        
+    public static void main(String[] args){
+        long ptr = Bridge.CLibrary.instance.BaseRobot_create(robotStarted, robotEnabled, robotDisabled, enabledPeriodic, disabledPeriodic, periodic, 10, 50, 100, 50);
     }
+
+    static Bridge.vptr_t robotStarted = new Bridge.vptr_t(){
+        @Override
+        public void invoke(){
+
+        }
+    };
+    static Bridge.vptr_t robotEnabled = new Bridge.vptr_t(){
+        @Override
+        public void invoke(){
+
+        }
+    };
+    static Bridge.vptr_t robotDisabled = new Bridge.vptr_t(){
+        @Override
+        public void invoke(){
+
+        }
+    };
+    static Bridge.vptr_t enabledPeriodic = new Bridge.vptr_t(){
+        @Override
+        public void invoke(){
+
+        }
+    };
+    static Bridge.vptr_t disabledPeriodic = new Bridge.vptr_t(){
+        @Override
+        public void invoke(){
+
+        }
+    };
+    static Bridge.vptr_t periodic = new Bridge.vptr_t(){
+        @Override
+        public void invoke(){
+
+        }
+    };
 }
