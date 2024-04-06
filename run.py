0cac8b431e2f58ba4664c81b29b0d0776f80f127
Marcus Behel <marcus.behel@mindspring.com>
2024-04-06 10:48:25 -0400

    Fix run.py library path when running python

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ import os
 import platform
 import argparse
 
+if platform.system() != "Linux":
+    print("Can only run on linux system!")
+
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 parser = argparse.ArgumentParser(description="Run testrobot program on this PC. Notice: you must build the cpp library for this pc before running the testrobot programs.")
@@ -13,19 +16,15 @@ args = parser.parse_args()
 
 try:
     if args.program == "cpp":
-        if platform.system() != "Windows":
-            if "LD_LIBRARY_PATH" not in os.environ:
-                os.environ["LD_LIBRARY_PATH"] = ""
-            os.environ["LD_LIBRARY_PATH"] += os.pathsep + os.path.join(script_dir, "cpp_library", "build", "native", args.config)
+        if "LD_LIBRARY_PATH" not in os.environ:
+            os.environ["LD_LIBRARY_PATH"] = ""
+        os.environ["LD_LIBRARY_PATH"] += os.pathsep + os.path.join(script_dir, "cpp_library", "build", "native", args.config)
         os.system(os.path.join("cpp_library", "build", "native", args.config, "testrobot.exe" if platform.system() == "Windows" else "testrobot"))
     elif args.program == "python":
         os.environ["PYTHONPATH"] = os.path.join(script_dir, "python_bindings")
-        if platform.system() == "Windows":
-            os.environ["PATH"] = os.path.join(script_dir, "cpp_library", "build", "native", args.config) + os.pathsep + os.environ["PATH"]
-        else:
-            if "LD_LIBRARY_PATH" not in os.environ:
-                os.environ["LD_LIBRARY_PATH"] = ""
-            os.environ["LD_LIBRARY_PATH"] += os.pathsep + os.path.join(script_dir, "cpp_library", "build")
+        if "LD_LIBRARY_PATH" not in os.environ:
+            os.environ["LD_LIBRARY_PATH"] = ""
+        os.environ["LD_LIBRARY_PATH"] += os.pathsep + os.path.join(script_dir, "cpp_library", "build", "native", args.config)
         os.system("python3 " + os.path.join("python_bindings", "testrobot-py", "main.py"))
 except KeyboardInterrupt:
     pass
