e74d8a1c1be30dd8c8f0d2b52c7743af320f8308
Marcus Behel <marcus.behel@mindspring.com>
2024-04-18 19:42:31 -0400

    Improve finds scripts for pigpio and lgpio

--- cpp_library/cmake/Findlgpio.cmake
+++ cpp_library/cmake/Findlgpio.cmake
@@ -21,6 +21,13 @@ set(lgpio_INCLUDES     ${lgpio_INCLUDE_DIR})
 # Handle REQUIRED, QUIET, and version arguments 
 # and set the <packagename>_FOUND variable.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(lgpio 
-    DEFAULT_MSG 
-    lgpio_INCLUDE_DIR lgpio_LIBRARY)
+find_package_handle_standard_args(lgpio REQUIRED_VARS lgpio_INCLUDE_DIR lgpio_LIBRARY)
+
+if(lgpio_FOUND)
+    if(NOT TARGET lgpio::lgpio)
+        add_library(lgpio::lgpio INTERFACE IMPORTED)
+        set_target_properties(lgpio::lgpio PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${lgpio_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES "${lgpio_LIBRARY}")
+    endif()
+endif()
--- cpp_library/cmake/Findpigpio.cmake
+++ cpp_library/cmake/Findpigpio.cmake
@@ -4,19 +4,19 @@
 
 # Find the path to the pigpio includes.
 find_path(pigpio_INCLUDE_DIR 
-	NAMES pigpio.h pigpiod_if.h pigpiod_if2.h
-	HINTS /usr/include /usr/local/include)
-	
+    NAMES pigpio.h pigpiod_if.h pigpiod_if2.h
+    HINTS /usr/include /usr/local/include)
+    
 # Find the pigpio libraries.
 find_library(pigpio_LIBRARY 
-	NAMES libpigpio.so
-	HINTS /usr/lib /usr/local/lib)
+    NAMES libpigpio.so
+    HINTS /usr/lib /usr/local/lib)
 find_library(pigpiod_if_LIBRARY 
-	NAMES libpigpiod_if.so
-	HINTS /usr/lib /usr/local/lib)
+    NAMES libpigpiod_if.so
+    HINTS /usr/lib /usr/local/lib)
 find_library(pigpiod_if2_LIBRARY 
-	NAMES libpigpiod_if2.so
-	HINTS /usr/lib /usr/local/lib)
+    NAMES libpigpiod_if2.so
+    HINTS /usr/lib /usr/local/lib)
     
 # Set the pigpio variables to plural form to make them accessible for 
 # the paramount cmake modules.
@@ -26,6 +26,25 @@ set(pigpio_INCLUDES     ${pigpio_INCLUDE_DIR})
 # Handle REQUIRED, QUIET, and version arguments 
 # and set the <packagename>_FOUND variable.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(pigpio 
-    DEFAULT_MSG 
-    pigpio_INCLUDE_DIR pigpio_LIBRARY pigpiod_if_LIBRARY pigpiod_if2_LIBRARY)
+find_package_handle_standard_args(pigpio REQUIRED_VARS pigpio_INCLUDE_DIR pigpio_LIBRARY pigpiod_if_LIBRARY pigpiod_if2_LIBRARY)
+
+if(pigpio_FOUND)
+    if(NOT TARGET pigpio::pigpio)
+        add_library(pigpio::pigpio INTERFACE IMPORTED)
+        set_target_properties(pigpio::pigpio PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${pigpio_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES "${pigpio_LIBRARY}")
+    endif()
+	if(NOT TARGET pigpio::pigpio_if)
+        add_library(pigpio::pigpio_if INTERFACE IMPORTED)
+        set_target_properties(pigpio::pigpio_if PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${pigpio_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES "${pigpiod_if_LIBRARY}")
+    endif()
+	if(NOT TARGET pigpio::pigpio_if2)
+        add_library(pigpio::pigpio_if2 INTERFACE IMPORTED)
+        set_target_properties(pigpio::pigpio_if2 PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${pigpio_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES "${pigpiod_if2_LIBRARY}")
+    endif()
+endif()
