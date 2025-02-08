2a7f3c090e1d7be96e104098b233faa89e53eda0
Marcus Behel <marcus.behel@mindspring.com>
2025-02-08 09:50:49 -0500

    Improve debugger functions in python code

--- python_bindings/arpirobot/util.py
+++ python_bindings/arpirobot/util.py
@@ -1,6 +1,20 @@
 
 import sys
 
+## Check if running under debugger
+def _is_under_debugger():
+    has_trace = hasattr(sys, 'gettrace') and sys.gettrace() is not None
+    has_breakpoint = sys.breakpointhook.__module__ != "sys"
+    return has_trace or has_breakpoint
+
+
+## Used in main.py to break debugger on entry
+def _break_debugger():
+    if _is_under_debugger() and 'debugpy' in sys.modules:
+        import debugpy
+        debugpy.breakpoint()
+
+
 # debugpy is not able to handle breakpoints in threads created in the C++ corelib
 # Since these threads are where most user code runs, this essentially makes debugging
 # python robot programs useless. Thus, we need to be able to debug such threads.
@@ -10,10 +24,6 @@ import sys
 # other than the main thread) is run. Thus, this function can be used in any
 # such callbacks.
 def _enable_debugpy_this_thread():
-    if not 'debugpy' in sys.modules:
-        return
-    try:
+    if _is_under_debugger() and 'debugpy' in sys.modules:
         import debugpy
         debugpy.debug_this_thread()
-    except:
-        pass
